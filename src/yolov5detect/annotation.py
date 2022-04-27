be90f09cb92706088c526c7d37d8e6a1a63393ad
bruce <2432001677@qq.com>
2022-04-27 11:24:20 +0800

    support modify annotate font scale

--- src/yolov5detect/annotation.py
+++ src/yolov5detect/annotation.py
@@ -75,7 +75,7 @@ class Annotator:
         self.lw = line_width or max(
             round(sum(im.shape) / 2 * 0.003), 2)  # line width
 
-    def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
+    def box_label(self, box, label='', color=(128, 128, 128), txt_color=(255, 255, 255),font_scale=0.5):
         # Add one xyxy box to image with label
         if self.pil or False:
             self.draw.rectangle(box, width=self.lw, outline=color)  # box
@@ -97,12 +97,12 @@ class Annotator:
                 tf = max(self.lw - 1, 1)  # font thickness
                 # text width, height
                 w, h = cv2.getTextSize(
-                    label, 0, fontScale=self.lw / 3, thickness=tf)[0]
+                    label, 0, fontScale=self.lw *font_scale, thickness=tf)[0]
                 outside = p1[1] - h - 3 >= 0  # label fits outside box
                 p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
                 cv2.rectangle(self.im, p1, p2, color, -
                               1, cv2.LINE_AA)  # filled
-                cv2.putText(self.im, label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2), 0, self.lw / 3, txt_color,
+                cv2.putText(self.im, label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2), 0, self.lw*font_scale, txt_color,
                             thickness=tf, lineType=cv2.LINE_AA)
         return np.asarray(self.im)
 
